2e3ea63b41625e647e8373e92347fc74261503d6
Vladimir <vgutman95@mail.ru>
2021-11-26 17:24:35 +0600

    add new styles

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', ()=>{
     const citySelect = document.querySelector('#city');
     let wrapperAllElement = document.querySelector('.list_result'),
         arrElements = wrapperAllElement.querySelectorAll('.item');
-    console.log(arrElements)
     citySelect.addEventListener('change', (e)=>{
         for(let i = 0; i < arrElements.length; i++){
             if(arrElements[i].dataset.city != citySelect.value){
@@ -48,4 +47,45 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
         }
     })
+
+
+//    slider
+
+    $('.wrapper_slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '.btn_prev',
+        nextArrow: '.btn_next',
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
 });
