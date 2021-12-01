dcf42b9be9027434dc19edd713126fe4b0715496
Vladimir <vgutman95@mail.ru>
2021-12-01 10:53:48 +0600

    add fix

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
    const burgerBtn = document.querySelector('.burger_menu_btn'),
        burgerMenu = document.querySelector('.burger_menu');
    burgerBtn.addEventListener('click', (e)=>{
+       e.preventDefault();
        burgerMenu.classList.toggle('burger_menu_active');
        if(burgerMenu.classList.contains('burger_menu_active')){
           burgerBtn.classList.add('burger_menu_btn_active');
